4649c81d1e9d5fa738423d2256607aff590bfe9e
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-04-18 16:56:23 +1000

    This will add profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :reviews, dependent: :destroy
 
+  has_attached_file :avatar, styles: { medium: "250x250#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   after_create :assign_default_role
   ratyrate_rater
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417131808) do
+ActiveRecord::Schema.define(version: 20170418052251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,6 +126,10 @@ ActiveRecord::Schema.define(version: 20170417131808) do
     t.string   "provider"
     t.string   "uid"
     t.string   "slug"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
