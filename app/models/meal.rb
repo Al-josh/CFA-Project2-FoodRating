14fde39431562c65046f4332f8f3a7e7e133654b
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-04-24 21:01:45 +1000

    this will connect paperclip to cloudinary

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :restaurant
   has_many :comments
-  has_attached_file :image, styles: {large: "300x300#", medium: "200x200#", thumb: "100x100#"}
+  has_attached_file :image, styles: {large: "300x300#", medium: "200x200#", thumb: "100x100#"}, :storage => :cloudinary, :path => ":id/:style/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   ratyrate_rateable "healthy"
 
