aec7a629ce3218ef439521629d698bbbda11ff95
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-04-25 13:09:57 +1000

    final styling

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :meals
-  has_attached_file :image, styles: {large: "400x400#", restolarge: "450x350#", medium: "200x200#", thumb: "100x100#"}, :storage => :cloudinary, :path => ":id/:style/:filename"
+  has_attached_file :image, styles: {large: "400x400#", restolarge: "290x290#", medium: "200x200#", thumb: "100x100#"}, :storage => :cloudinary, :path => ":id/:style/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   geocoded_by :address
